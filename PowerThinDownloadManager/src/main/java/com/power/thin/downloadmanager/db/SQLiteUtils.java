f64aa5c3953105cb6ecd402b286f830a55b4e388
wangzhengyi <wzyll1314@gmail.com>
2016-12-12 11:19:11 +0800

    完善数据库的增删操作

--- PowerThinDownloadManager/src/main/java/com/power/thin/downloadmanager/db/SQLiteUtils.java
+++ PowerThinDownloadManager/src/main/java/com/power/thin/downloadmanager/db/SQLiteUtils.java
@@ -7,6 +7,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class SQLiteUtils {
     private DatabaseHelper mDatabaseHelper;
 
@@ -40,7 +44,7 @@ public class SQLiteUtils {
                     new String[]{String.valueOf(downloadInfoBean.getRequestId())});
             if (cursor != null && cursor.moveToFirst()) {
                 db.update(DownloadInfoTable.TABLE_NAME, cv, DownloadInfoTable.REQUEST_ID + "= ?",
-                        new String[] {String.valueOf(downloadInfoBean.getRequestId())});
+                        new String[]{String.valueOf(downloadInfoBean.getRequestId())});
             } else {
                 db.insert(DownloadInfoTable.TABLE_NAME, null, cv);
             }
@@ -52,5 +56,100 @@ public class SQLiteUtils {
                 db.close();
             }
         }
+    }
+
+    public DownloadInfoBean getDownloadInfo(int requestId) {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        DownloadInfoBean bean = null;
+        Cursor cursor = null;
+        try {
+            cursor = db.rawQuery("SELECT * FROM" + DownloadInfoTable.TABLE_NAME
+                            + " WHERE " + DownloadInfoTable.REQUEST_ID + " = ?",
+                    new String[]{String.valueOf(requestId)});
+            if (cursor != null && cursor.moveToFirst()) {
+                bean = new DownloadInfoBean();
+                bean.setRequestId(requestId);
+                bean.setDownloadUrl(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.DOWNLOAD_URL)));
+                bean.setDestinationPath(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.DESTINATION_FILE_PATH)));
+                bean.setTotalSize(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.TOTAL_SIZE)));
+                bean.setDownloadSize(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.DOWNLOAD_SIZE)));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null) {
+                db.close();
+            }
+        }
+
+        return bean;
+    }
+
+    public List<DownloadInfoBean> getAllDownloadInfo() {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        Cursor cursor = null;
+        List<DownloadInfoBean> downloadInfoBeanList = new ArrayList<DownloadInfoBean>();
+        try {
+            cursor = db.rawQuery("SELECT * FROM" + DownloadInfoTable.TABLE_NAME, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                DownloadInfoBean bean = new DownloadInfoBean();
+                bean.setRequestId(cursor.getInt(
+                        cursor.getColumnIndex(DownloadInfoTable.REQUEST_ID)));
+                bean.setDownloadUrl(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.DOWNLOAD_URL)));
+                bean.setDestinationPath(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.DESTINATION_FILE_PATH)));
+                bean.setTotalSize(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.TOTAL_SIZE)));
+                bean.setDownloadSize(cursor.getString(
+                        cursor.getColumnIndex(DownloadInfoTable.DOWNLOAD_SIZE)));
+                downloadInfoBeanList.add(bean);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null) {
+                db.close();
+            }
+        }
+
+        return downloadInfoBeanList;
+    }
+
+    public int deleteDownloadInfo(int requestId) {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        int affectedNum = 0;
+        try {
+            affectedNum = db.delete(
+                    DownloadInfoTable.TABLE_NAME,
+                    DownloadInfoTable.REQUEST_ID + " = ?",
+                    new String[]{String.valueOf(requestId)});
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+        }
+
+        return affectedNum;
+    }
+
+    public int deleteAllDownloadInfo() {
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+        int affectedNum = 0;
+        try {
+            affectedNum = db.delete(DownloadInfoTable.TABLE_NAME, null, null);
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+        }
+
+        return affectedNum;
     }
 }
